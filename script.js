d62a8f86e2621930edf7d822450ddd500ef0f0a5
Joseph <jwillgerona@gmail.com>
2023-01-17 18:17:10 -0800

    cleaned files of uneccessary comments, optimized js

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
 // Assignment code here
-const specialChars = [' ','~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', '/', ':', ';', '"', '\'', '<', '>', ',', '.', '?'];
+const specialChars = [' ', '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', '/', ':', ';', '"', '\'', '<', '>', ',', '.', '?'];
 const lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+//pass in the length of desired password, then a list of the desired characters for the password
 function writePassword(passLength, charIncluded) {
-  // var password = generatePassword();
+
   var passwordText = document.getElementById("password");
-
-  // passwordText.value = password;
   var password = "";
-  // var index = Math.floor(Math.random() * options.length);
-  // var computerChoice = options[index];
-  //var index = Math.floor(Math.random() * charIncluded.length);
-
   for (var i = 0; i < passLength; i++) {
     var index = Math.floor(Math.random() * charIncluded.length);
     password += charIncluded[index];
-    passwordText.setAttribute("placeholder", password);
   }
-  // console.log(password[0]);
-  // console.log(password[1]);
-  // passwordText.placeholder = password.valueOf();
-  // passwordText.placeholder = password[1];
-  // console.log(password);
-
+  passwordText.setAttribute("placeholder", password);
 }
 
 function myPrompt() {
@@ -54,8 +39,7 @@ function myPrompt() {
       charIncluded = charIncluded.concat(specialChars);
     }
     //if true, create a password given the desired characters.
-    //if (charIncluded != "") {
-    if (isLowerIncluded || isUpperIncluded || isNumericIncluded || isSpecialIncluded) {
+    if (charIncluded != "") {
       writePassword(passLength, charIncluded);
     } else {
       alert("Please select at least one of the options.");
@@ -64,7 +48,3 @@ function myPrompt() {
     alert("Please type a number between 8 and 124");
   }
 }
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
